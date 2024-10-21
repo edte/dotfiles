f42bab132358365b6c80fcf3e038fbb3e15f2ce3
edte <zzzzip6@gmail.com>
2024-10-21 20:21:44 +0800

    update

--- nvim/lua/ui/plugins.lua
+++ nvim/lua/ui/plugins.lua
@@ -1,7 +1,6 @@
 local M = {}
 
 M.list = {
-
     {
         "folke/tokyonight.nvim",
         lazy = false,
@@ -16,57 +15,76 @@ M.list = {
         },
     },
 
-    --    {
-    --        "morhetz/gruvbox",
-    --        config = function()
-    --            vim.cmd([[
-    -- "Use 24-bit (true-color) mode in Vim/Neovim when outside tmux.
-    -- "If you're using tmux version 2.2 or later, you can remove the outermost $TMUX check and use tmux's 24-bit color support
-    -- "(see < http://sunaku.github.io/tmux-24bit-color.html#usage > for more information.)
-    -- if (empty($TMUX) && getenv('TERM_PROGRAM') != 'Apple_Terminal')
-    --   if (has("nvim"))
-    --     "For Neovim 0.1.3 and 0.1.4 < https://github.com/neovim/neovim/pull/2198 >
-    --     let $NVIM_TUI_ENABLE_TRUE_COLOR=1
-    --   endif
-    --   "For Neovim > 0.1.5 and Vim > patch 7.4.1799 < https://github.com/vim/vim/commit/61be73bb0f965a895bfb064ea3e55476ac175162 >
-    --   "Based on Vim patch 7.4.1770 (`guicolors` option) < https://github.com/vim/vim/commit/8a633e3427b47286869aa4b96f2bfc1fe65b25cd >
-    --   " < https://github.com/neovim/neovim/wiki/Following-HEAD#20160511 >
-    --   if (has("termguicolors"))
-    --     set termguicolors
-    --   endif
-    -- endif
-    --       ]])
-    --
-    --            vim.cmd([[colorscheme gruvbox]])
-    --        end,
-    --    },
-
     -- {
-    -- 	"joshdick/onedark.vim",
-    -- 	config = function()
-    -- 		vim.cmd([[colorscheme onedark]])
-    -- 	end,
+    --     "catppuccin/nvim",
+    --     config = function()
+    --         vim.cmd([[colorscheme catppuccin]])
+    --     end,
     -- },
 
     -- {
-    -- 	"catppuccin/nvim",
-    -- 	config = function()
-    -- 		vim.cmd([[colorscheme catppuccin]])
-    -- 	end,
+    --     "rebelot/kanagawa.nvim",
+    --     config = function()
+    --         vim.cmd([[colorscheme kanagawa]])
+    --     end,
     -- },
 
-    -- {
-    -- 	"rebelot/kanagawa.nvim",
-    -- 	config = function()
-    -- 		vim.cmd([[colorscheme kanagawa]])
-    -- 	end,
-    -- },
+    -- 上方的tab栏
+    {
+        dir = "ui.tabline",
+        version = false,
+        config = function()
+            require("ui.tabline").setup()
+        end,
+    },
 
-    -- 自定义状态栏
+
+    -- 下方的状态栏
     {
         dir = "ui.statusline",
         config = function()
             require("ui.statusline")
+        end,
+    },
+
+    -- 右边的滚动条
+    {
+        "lewis6991/satellite.nvim",
+        opts = {
+            handlers = {
+                marks = {
+                    enable = false,
+                },
+            },
+        },
+    },
+
+
+    -- 不是天空中的 UFO，而是 Neovim 中的超级折叠。 za
+    {
+        "kevinhwang91/nvim-ufo",
+        dependencies = "kevinhwang91/promise-async",
+        opts = require("ui.fold").Opts,
+        init = require("ui.fold").init,
+        config = require("ui.fold").config,
+    },
+
+    -- 左边的状态列
+    {
+        "luukvbaal/statuscol.nvim",
+        config = function()
+            local builtin = require("statuscol.builtin")
+            require("statuscol").setup({
+                -- relculright = true,
+                segments = {
+                    -- marks
+                    { sign = { name = { ".*" }, maxwidth = 2, colwidth = 1, auto = true, wrap = false } },
+                    -- 行号
+                    { text = { builtin.lnumfunc } },
+                    -- 折叠
+                    { text = { builtin.foldfunc } },
+                },
+            })
         end,
     },
 
@@ -156,17 +174,6 @@ M.list = {
         requires = { "kyazdani42/nvim-web-devicons" },
         config = function()
             require("ui.dashboard").config()
-        end,
-    },
-
-    -- 迷你 bufferline
-    {
-        "edte/mini.tabline",
-        version = false,
-        config = function()
-            require("mini.tabline").setup({})
-            keymap("n", "gn", "<cmd>bn<CR>")
-            keymap("n", "gp", "<cmd>bp<CR>")
         end,
     },
 
@@ -264,46 +271,7 @@ M.list = {
         },
     },
 
-    -- Neovim 的装饰滚动条
-    {
-        "lewis6991/satellite.nvim",
-        opts = {
-            handlers = {
-                marks = {
-                    enable = false,
-                },
-            },
-        },
-    },
 
-    -- 不是天空中的 UFO，而是 Neovim 中的超级折叠。 za
-    {
-        "kevinhwang91/nvim-ufo",
-        dependencies = "kevinhwang91/promise-async",
-        opts = require("ui.fold").Opts,
-        init = require("ui.fold").init,
-        config = require("ui.fold").config,
-    },
-
-    -- 状态列插件，提供可配置的“状态列”和单击处理程序。需要 Neovim >= 0.10。
-    -- statuscolumn
-    {
-        "luukvbaal/statuscol.nvim",
-        config = function()
-            local builtin = require("statuscol.builtin")
-            require("statuscol").setup({
-                -- relculright = true,
-                segments = {
-                    -- marks
-                    { sign = { name = { ".*" }, maxwidth = 2, colwidth = 1, auto = true, wrap = false } },
-                    -- 行号
-                    { text = { builtin.lnumfunc } },
-                    -- 折叠
-                    { text = { builtin.foldfunc } },
-                },
-            })
-        end,
-    },
 
     {
         dir = "ui.tmux",
