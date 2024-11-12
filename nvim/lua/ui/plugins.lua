3e96675467d37f70e5fd8a0923867cc0b21fabed
edte <zzzzip6@gmail.com>
2024-11-12 15:30:15 +0800

    update

--- nvim/lua/ui/plugins.lua
+++ nvim/lua/ui/plugins.lua
@@ -60,14 +60,14 @@ M.list = {
     },
 
 
-    -- -- 不是天空中的 UFO，而是 Neovim 中的超级折叠。 za
-    -- {
-    --     "kevinhwang91/nvim-ufo",
-    --     dependencies = "kevinhwang91/promise-async",
-    --     opts = require("ui.fold").Opts,
-    --     init = require("ui.fold").init,
-    --     config = require("ui.fold").config,
-    -- },
+    -- 不是天空中的 UFO，而是 Neovim 中的超级折叠。 za
+    {
+        "kevinhwang91/nvim-ufo",
+        dependencies = "kevinhwang91/promise-async",
+        opts = require("ui.fold").Opts,
+        init = require("ui.fold").init,
+        config = require("ui.fold").config,
+    },
 
     -- 左边的状态列
     {
@@ -86,21 +86,21 @@ M.list = {
                 },
             })
 
-            require("ui.fold").init()
-            vim.o.foldmethod = 'expr'
-            vim.o.foldexpr = 'nvim_treesitter#foldexpr()'
-            vim.o.foldlevelstart = 99
-
-            vim.cmd([[set viewoptions-=curdir]])
-
-            -- remember folds
-            vim.cmd([[
-	       augroup remember_folds
-	       autocmd!
-	       autocmd BufWinLeave *.* mkview
-	       autocmd BufWinEnter *.* silent! loadview
-	       augroup END
-	       ]])
+            --     require("ui.fold").init()
+            --     vim.o.foldmethod = 'expr'
+            --     vim.o.foldexpr = 'nvim_treesitter#foldexpr()'
+            --     vim.o.foldlevelstart = 99
+            --
+            --     vim.cmd([[set viewoptions-=curdir]])
+            --
+            --     -- remember folds
+            --     vim.cmd([[
+            -- augroup remember_folds
+            -- autocmd!
+            -- autocmd BufWinLeave *.* mkview
+            -- autocmd BufWinEnter *.* silent! loadview
+            -- augroup END
+            -- ]])
         end,
     },
 
