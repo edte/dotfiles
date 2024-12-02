b3a35d3e6483a3fccae48889759663463841a3dc
edte <zzzzip6@gmail.com>
2024-12-02 15:40:06 +0800

    update

--- nvim/lua/lsp/plugins.lua
+++ nvim/lua/lsp/plugins.lua
@@ -36,25 +36,6 @@ M.list = {
         },
     },
 
-    -- lsp_lines 是一个简单的 neovim 插件，它使用真实代码行之上的虚拟行来呈现诊断。
-    --https://git.sr.ht/~whynothugo/lsp_lines.nvim
-    -- {
-    --     -- url 备份
-    --     -- url = "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
-    --     url = "https://github.com/edte/lsp_lines.nvim",
-    --     config = function()
-    --         vim.diagnostic.config({
-    --             virtual_text = false,
-    --             update_in_insert = true,
-    --             virtual_lines = {
-    --                 -- only_current_line = true,
-    --                 highlight_whole_line = false,
-    --             },
-    --         })
-    --         require("lsp_lines").setup()
-    --         vim.keymap.set("n", "g?", vim.diagnostic.open_float, { silent = true })
-    --     end,
-    -- },
 
     -- Clanalphagd 针对 neovim 的 LSP 客户端的不合规范的功能。使用 https://sr.ht/~p00f/clangd_extensions.nvim 代替
     {
@@ -315,13 +296,70 @@ M.list = {
     --     }
     -- },
 
+
+    -- lsp_lines 是一个简单的 neovim 插件，它使用真实代码行之上的虚拟行来呈现诊断。
+    --https://git.sr.ht/~whynothugo/lsp_lines.nvim
+    -- {
+    --     -- url 备份
+    --     -- url = "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
+    --     url = "https://github.com/edte/lsp_lines.nvim",
+    --     config = function()
+    --         vim.diagnostic.config({
+    --             virtual_text = false,
+    --             update_in_insert = true,
+    --             virtual_lines = {
+    --                 -- only_current_line = true,
+    --                 highlight_whole_line = false,
+    --             },
+    --         })
+    --         require("lsp_lines").setup()
+    --         vim.keymap.set("n", "g?", vim.diagnostic.open_float, { silent = true })
+    --     end,
+    -- },
+
     {
         "rachartier/tiny-inline-diagnostic.nvim",
-        event = "LspAttach", -- Or `LspAttach`
-        priority = 1000,     -- needs to be loaded in first
+        -- event = "LspAttach", -- Or `LspAttach`
+        priority = 3000, -- needs to be loaded in first
         config = function()
-            require('tiny-inline-diagnostic').setup()
-            vim.diagnostic.config({ virtual_text = false })
+            -- Default configuration
+            require("tiny-inline-diagnostic").setup({
+                options = {
+                    -- Throttle the update of the diagnostic when moving cursor, in milliseconds.
+                    -- You can increase it if you have performance issues.
+                    -- Or set it to 0 to have better visuals.
+                    throttle = 0,
+
+                    -- The minimum length of the message, otherwise it will be on a new line.
+                    softwrap = 50,
+
+                    -- If multiple diagnostics are under the cursor, display all of them.
+                    multiple_diag_under_cursor = true,
+
+                    -- Enable diagnostic message on all lines.
+                    multilines = true,
+
+                    -- Show all diagnostics on the cursor line.
+                    show_all_diags_on_cursorline = true,
+
+                    -- Enable diagnostics on Insert mode. You should also se the `throttle` option to 0, as some artefacts may appear.
+                    enable_on_insert = true,
+
+                    -- Format the diagnostic message.
+                    format = function(diagnostic)
+                        return diagnostic.message
+                    end,
+
+                },
+            })
+            vim.diagnostic.config({
+                virtual_text = false,
+                update_in_insert = true,
+                virtual_lines = {
+                    -- only_current_line = true,
+                    highlight_whole_line = false,
+                },
+            })
         end
     }
 }
