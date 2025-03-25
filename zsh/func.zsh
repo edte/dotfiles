6eae8161c4dc82dd1c2eb5f3dbeaae12b17072b1
edte <zzzzip6@gmail.com>
2025-03-25 16:18:10 +0800

    update

--- zsh/func.zsh
+++ zsh/func.zsh
@@ -13,7 +13,7 @@ function gitbranch() {
 }
 
 function backup() {
-    cd ~/dotfiles/ && git add . && git commit -m "update" && git push origin master
+    cd ~/dotfiles/ && git add . && git commit -m "update" && git push origin master && cd -
 }
 
 function update() {
