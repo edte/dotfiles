5220d96b96e1e6b0a643e7b3c3a749be1768e691
edte <zzzzip6@gmail.com>
2025-06-30 20:46:25 +0800

    refactor: 移除多余插件与依赖，调整默认补全源

--- nvim/lua/cmp/plugins.lua
+++ nvim/lua/cmp/plugins.lua
@@ -9,198 +9,19 @@ M.list = {
 		opts = {},
 	},
 
-	-- nvim lua  源
-	-- {
-	-- 	"hrsh7th/cmp-nvim-lua",
-	-- 	event = "InsertEnter *.lua",
-	-- },
-
 	-- 自定义代码片段
 	{
 		"L3MON4D3/LuaSnip",
 		lazy = true,
 		event = "InsertEnter",
 		config = function()
-			-- require("luasnip.loaders.from_vscode").lazy_load()
 			require("luasnip.loaders.from_lua").load({ paths = NEOVIM_CONFIG_PATH .. "/lua/cmp/luasnippets" })
 		end,
 		opts = {
 			history = true,
 			delete_check_events = "TextChanged",
 		},
-		dependencies = {
-			-- "rafamadriz/friendly-snippets",
-		},
 	},
-
-	-- cmp 补全基础插件
-	-- {
-	-- 	"hrsh7th/nvim-cmp",
-	-- 	event = { "InsertEnter" },
-	-- 	config = function()
-	-- 		require("cmp.cmp").setup()
-	-- 	end,
-	-- 	dependencies = {
-	-- 		-- 下面是一堆cmp补全源
-	-- 		-- 只能所有文件都用的源才放这里，单独的源放外面，避免多余的加载
-	-- 		{
-	-- 			"hrsh7th/cmp-buffer",
-	-- 			event = { "InsertEnter" },
-	-- 		},
-	-- 		{
-	-- 			"hrsh7th/cmp-nvim-lsp",
-	-- 			event = { "InsertEnter" },
-	-- 		},
-	--
-	-- 		{
-	-- 			"samiulsami/cmp-go-deep",
-	-- 			event = { "InsertEnter" },
-	-- 		},
-	--
-	-- 		-- {
-	-- 		-- 	"Exafunction/windsurf.nvim",
-	-- 		-- 	cmd = "Codeium",
-	-- 		-- 	event = "InsertEnter",
-	-- 		-- 	build = ":Codeium Auth",
-	-- 		-- 	opts = {
-	-- 		-- 		enable_cmp_source = vim.g.ai_cmp,
-	-- 		-- 		virtual_text = {
-	-- 		-- 			enabled = not vim.g.ai_cmp,
-	-- 		-- 			key_bindings = {
-	-- 		-- 				accept = true, -- handled by nvim-cmp / blink.cmp
-	-- 		-- 				-- next = "<M-]>",
-	-- 		-- 				-- prev = "<M-[>",
-	-- 		-- 			},
-	-- 		-- 		},
-	-- 		-- 	},
-	-- 		-- 	dependencies = {
-	-- 		-- 		"nvim-lua/plenary.nvim",
-	-- 		-- 		"hrsh7th/nvim-cmp",
-	-- 		-- 	},
-	-- 		-- 	config = function()
-	-- 		-- 		require("codeium").setup({})
-	-- 		-- 	end,
-	-- 		-- },
-	--
-	-- 		{
-	-- 			"edte/cmp-go-pkgs.nvim",
-	-- 			event = { "InsertEnter *.go" },
-	-- 		},
-	--
-	-- 		{
-	-- 			"saadparwaiz1/cmp_luasnip",
-	-- 			event = { "InsertEnter" },
-	-- 		},
-	-- 		-- 路径补全，还是有用的
-	-- 		{
-	-- 			"edte/cmp-async-path",
-	-- 			event = { "InsertEnter" },
-	-- 		},
-	-- 		{
-	-- 			"edte/cmp-nvim-lsp-signature-help",
-	-- 			event = { "InsertEnter" },
-	-- 		},
-	--
-	-- 		{
-	-- 			"chrisgrieser/cmp_yanky",
-	-- 			event = { "InsertEnter" },
-	-- 		},
-	-- 		-- -- 上下文语法补全
-	-- 		{
-	-- 			"ray-x/cmp-treesitter",
-	-- 			event = { "InsertEnter" },
-	-- 		},
-	-- 		{
-	-- 			"edte/cmp-rg",
-	-- 			event = { "InsertEnter" },
-	-- 			lazy = true,
-	-- 			enabled = function()
-	-- 				return vim.fn.executable("rg") == 1
-	-- 			end,
-	-- 		},
-	-- 		{
-	-- 			"ray-x/cmp-shellcmds-history",
-	-- 			config = function()
-	-- 				require("cmp_shellcmds_history").setup({
-	-- 					default_interval = 1200000, -- read/refresh history every 20 minutes
-	-- 					max_items = 10000, -- maximum number of items to read from history
-	-- 				})
-	-- 			end,
-	-- 		},
-	--
-	-- 		-- 单词补全
-	-- 		{
-	-- 			"uga-rosa/cmp-dictionary",
-	-- 			event = { "InsertEnter" },
-	-- 			config = function()
-	-- 				-- git clone https://github.com/skywind3000/vim-dict nvim/
-	-- 				local dict = {
-	-- 					["*"] = { "/usr/share/dict/words" },
-	-- 					["go"] = { NEOVIM_CONFIG_PATH .. "/lua/cmp/dict/go.dict" },
-	-- 					["sh"] = { NEOVIM_CONFIG_PATH .. "/lua/cmp/dict/sh.dict" },
-	-- 					["lua"] = { NEOVIM_CONFIG_PATH .. "/lua/cmp/dict/lua.dict" },
-	-- 					["html"] = { NEOVIM_CONFIG_PATH .. "/lua/cmp/dict/html.dict" },
-	-- 					["css"] = { NEOVIM_CONFIG_PATH .. "/lua/cmp/dict/css.dict" },
-	-- 					["cpp"] = { NEOVIM_CONFIG_PATH .. "/lua/cmp/dict/cpp.dict" },
-	-- 					["cmake"] = { NEOVIM_CONFIG_PATH .. "/lua/cmp/dict/cmake.dict" },
-	-- 					["c"] = { NEOVIM_CONFIG_PATH .. "/lua/cmp/dict/c.dict" },
-	-- 				}
-	--
-	-- 				local function get_dict_path(file)
-	-- 					local paths = {}
-	-- 					if file:find(".*xmake.lua") then
-	-- 						paths = dict.xmake
-	-- 					else
-	-- 						paths = dict[vim.bo.filetype] or {}
-	-- 					end
-	-- 					vim.list_extend(paths, dict["*"])
-	-- 					return paths
-	-- 				end
-	--
-	-- 				Setup("cmp_dictionary", {
-	-- 					paths = get_dict_path(vim.fn.expand("%")),
-	-- 					exact_length = 2,
-	-- 					first_case_insensitive = false,
-	-- 				})
-	--
-	-- 				Autocmd("BufEnter", {
-	-- 					pattern = "*",
-	-- 					callback = function(ev)
-	-- 						require("cmp_dictionary").setup({ paths = get_dict_path(ev.file) })
-	-- 					end,
-	-- 				})
-	-- 			end,
-	-- 		},
-	--
-	-- 		{
-	-- 			"tzachar/cmp-tabnine",
-	-- 			build = "./install.sh",
-	-- 			event = { "InsertEnter" },
-	-- 			-- ft = { "lua", "go", "cpp" },
-	-- 			config = function()
-	-- 				-- tabnine 设置，一个ai补全的
-	-- 				local tabnine = Require("cmp_tabnine.config")
-	-- 				if tabnine == nil then
-	-- 					return
-	-- 				end
-	-- 				tabnine:setup({
-	-- 					max_lines = 1000,
-	-- 					max_num_results = 4,
-	-- 					sort = true,
-	-- 					run_on_every_keystroke = true,
-	-- 					snippet_placeholder = "..",
-	-- 					ignored_file_types = {
-	-- 						-- default is not to ignore
-	-- 						-- uncomment to ignore in lua:
-	-- 						-- lua = true
-	-- 					},
-	-- 					show_prediction_strength = false,
-	-- 					min_percent = 0,
-	-- 				})
-	-- 			end,
-	-- 		},
-	-- 	},
-	-- },
 
 	-- 彩色补全
 	{
@@ -216,10 +37,6 @@ M.list = {
 	{
 		"edte/copilot",
 	},
-
-	-- {
-	-- 	"tpope/vim-endwise",
-	-- },
 
 	-- Neovim 插件可快速插入日志语句并捕获日志输出
 	-- glj	    :      在光标下方插入一条日志语句
@@ -259,45 +76,9 @@ M.list = {
 
 		dependencies = {
 			{
-				"saghen/blink.compat",
-				-- use the latest release, via version = '*', if you also use the latest release for blink.cmp
-				version = "*",
-				-- lazy.nvim will automatically load the plugin when it's required by blink.cmp
-				lazy = true,
-				-- make sure to set opts so that lazy.nvim calls blink.compat's setup
-				opts = {
-					debug = true,
-				},
-			},
-
-			-- {
-			-- 	"edte/blink-go-import.nvim",
-			-- 	ft = "go",
-			-- 	config = function()
-			-- 		require("blink-go-import").setup()
-			-- 	end,
-			-- },
-
-			{
-				"tzachar/cmp-tabnine",
-				build = "./install.sh",
-				event = { "InsertEnter" },
-				-- ft = { "lua", "go", "cpp" },
-			},
-			{
 				"niuiic/blink-cmp-rg.nvim",
 			},
 
-			-- {
-			-- 	"hrsh7th/cmp-nvim-lua",
-			-- 	event = { "InsertEnter *.lua" },
-			-- },
-
-			-- 上下文语法补全
-			-- {
-			-- 	"ray-x/cmp-treesitter",
-			-- 	event = { "InsertEnter" },
-			-- },
 			-- 自定义代码片段
 			{
 				"L3MON4D3/LuaSnip",
@@ -307,10 +88,6 @@ M.list = {
 					require("luasnip.loaders.from_lua").load({ paths = NEOVIM_CONFIG_PATH .. "/lua/cmp/luasnippets" })
 				end,
 			},
-			-- {
-			-- 	"edte/cmp-nvim-lsp-signature-help",
-			-- 	event = { "InsertEnter" },
-			-- },
 		},
 
 		config = function()
@@ -341,34 +118,13 @@ M.list = {
 				},
 
 				sources = {
-					-- default = function(ctx)
-					-- 	local success, node = pcall(vim.treesitter.get_node)
-					-- 	if
-					-- 		success
-					-- 		and node
-					-- 		and vim.tbl_contains({ "comment", "line_comment", "block_comment" }, node:type())
-					-- 	then
-					-- 		return { "buffer" }
-					-- 	elseif vim.bo.filetype == "go" then
-					-- 		return { "lsp", "path", "snippets", "buffer", "Tabnine", "ripgrep", "go_import" }
-					-- 	else
-					-- 		return { "lsp", "path", "snippets", "buffer", "Tabnine", "ripgrep" }
-					-- 	end
-					-- end,
-
 					per_filetype = {
 						codecompanion = { "codecompanion" },
 					},
 
-					default = { "lsp", "path", "snippets", "buffer", "Tabnine", "ripgrep", "go_import" },
+					default = { "lsp", "path", "snippets", "buffer", "ripgrep", "go_import" },
 
 					providers = {
-						-- signature = {
-						-- 	name = "nvim_lsp_signature_help",
-						-- 	module = "blink.compat.source",
-						-- 	score_offset = -9,
-						-- },
-
 						go_import = {
 							name = "Module",
 							module = "more-go.go-pkgs-blink",
@@ -401,28 +157,6 @@ M.list = {
 							min_keyword_length = 4,
 							score_offset = -3,
 						},
-						Tabnine = {
-							name = "cmp_tabnine",
-							module = "blink.compat.source",
-							score_offset = -4,
-
-							opts = {
-								max_lines = 1000,
-								max_num_results = 20,
-								sort = true,
-								run_on_every_keystroke = true,
-								snippet_placeholder = "..",
-								ignored_file_types = {},
-								show_prediction_strength = true,
-								min_percent = 0,
-							},
-						},
-
-						-- lazydev = {
-						-- 	name = "LazyDev",
-						-- 	module = "lazydev.integrations.blink",
-						-- 	score_offset = 100, -- show at a higher priority than lsp
-						-- },
 
 						ripgrep = {
 							module = "blink-cmp-rg",
@@ -449,18 +183,6 @@ M.list = {
 									return context.line:sub(1, context.cursor[2]):match("[%w_-]+$") or ""
 								end,
 							},
-						},
-
-						nvim_lua = {
-							name = "nvim_lua",
-							module = "blink.compat.source",
-							score_offset = -7,
-						},
-
-						treesitter = {
-							name = "treesitter",
-							module = "blink.compat.source",
-							score_offset = -8,
 						},
 					},
 				},
