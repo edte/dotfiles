00e3b73885603435ad2cb4a5932891f9f28c2f33
edte <zzzzip6@gmail.com>
2025-12-10 15:22:01 +0800

    feat: 为vscode-diff.nvim插件新增keymaps配置并调整codebuddy命令

--- nvim/lua/git/plugins.lua
+++ nvim/lua/git/plugins.lua
@@ -124,6 +124,23 @@ M.list = {
 		"esmuellert/vscode-diff.nvim",
 		dependencies = { "MunifTanjim/nui.nvim" },
 		cmd = "CodeDiff",
+		opts = {
+			keymaps = {
+				view = {
+					quit = "q", -- Close diff tab
+					toggle_explorer = "<space>e", -- Toggle explorer visibility (explorer mode only)
+					next_hunk = "]h", -- Jump to next change
+					prev_hunk = "[h", -- Jump to previous change
+					next_file = "]f", -- Next file in explorer mode
+					prev_file = "[f", -- Previous file in explorer mode
+				},
+				explorer = {
+					select = "l", -- Open diff for selected file
+					hover = "K", -- Show file diff preview
+					refresh = "R", -- Refresh git status
+				},
+			},
+		},
 		keys = {
 			{
 				"<space>d",
