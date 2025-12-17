86ad8c320f347d080701871215a1fc90738bf56b
edte <zzzzip6@gmail.com>
2025-12-17 16:44:06 +0800

    merge

--- nvim/lua/components/plugins.lua
+++ nvim/lua/components/plugins.lua
@@ -339,7 +339,16 @@ M.list = {
                 end,
             })
         end,
+    },
+    -- Neovim 的通用日志语法突出显示和文件类型管理
+    {
+        "fei6409/log-highlight.nvim",
+        ft = "log",
+        config = function()
+            require("log-highlight").setup({})
+        end,
     }
+
 
 
 }
