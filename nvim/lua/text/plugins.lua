1a1d0265ed294da76a73b372eac7a82542fc4c86
edte <zzzzip6@gmail.com>
2025-12-24 16:20:40 +0800

    feat: 启用并配置 treewalker.nvim 插件及快捷键

--- nvim/lua/text/plugins.lua
+++ nvim/lua/text/plugins.lua
@@ -86,30 +86,30 @@ M.list = {
 			require("hlargs").setup()
 		end,
 	},
-	-- {
-	-- 	"aaronik/treewalker.nvim",
-	--
-	-- 	-- The following options are the defaults.
-	-- 	-- Treewalker aims for sane defaults, so these are each individually optional,
-	-- 	-- and setup() does not need to be called, so the whole opts block is optional as well.
-	-- 	opts = {
-	-- 		-- Whether to briefly highlight the node after jumping to it
-	-- 		highlight = true,
-	--
-	-- 		-- How long should above highlight last (in ms)
-	-- 		highlight_duration = 250,
-	--
-	-- 		-- The color of the above highlight. Must be a valid vim highlight group.
-	-- 		-- (see :h highlight-group for options)
-	-- 		highlight_group = "CursorLine",
-	-- 	},
-	-- 	keys = {
-	-- 		{ "<up>", "<cmd>Treewalker Up<cr>", mode = { "n", "v" }, desc = "up" },
-	-- 		{ "<down>", "<cmd>Treewalker Down<cr>", mode = { "n", "v" }, desc = "down" },
-	-- 		{ "<left>", "<cmd>Treewalker Left<cr>", mode = { "n", "v" }, desc = "down" },
-	-- 		{ "<right>", "<cmd>Treewalker Right<cr>", mode = { "n", "v" }, desc = "down" },
-	-- 	},
-	-- },
+	{
+		"aaronik/treewalker.nvim",
+
+		-- The following options are the defaults.
+		-- Treewalker aims for sane defaults, so these are each individually optional,
+		-- and setup() does not need to be called, so the whole opts block is optional as well.
+		opts = {
+			-- Whether to briefly highlight the node after jumping to it
+			highlight = true,
+
+			-- How long should above highlight last (in ms)
+			highlight_duration = 250,
+
+			-- The color of the above highlight. Must be a valid vim highlight group.
+			-- (see :h highlight-group for options)
+			highlight_group = "CursorLine",
+		},
+		keys = {
+			{ "<c-k>", "<cmd>Treewalker Up<cr>", mode = { "n", "v" }, desc = "up" },
+			{ "<c-j>", "<cmd>Treewalker Down<cr>", mode = { "n", "v" }, desc = "down" },
+			-- { "<left>", "<cmd>Treewalker Left<cr>", mode = { "n", "v" }, desc = "down" },
+			-- { "<right>", "<cmd>Treewalker Right<cr>", mode = { "n", "v" }, desc = "down" },
+		},
+	},
 }
 
 return M
