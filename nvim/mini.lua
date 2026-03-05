b09c43eed962a26dd0a8ea94ef6e991b6662cb08
edte <zzzzip6@gmail.com>
2026-03-05 17:18:30 +0800

    feat: 更新 lazy.nvim 配置并添加 copilot 插件

--- nvim/mini.lua
+++ nvim/mini.lua
@@ -1,7 +1,11 @@
 -- 最小配置
 
 vim.pack.add({
-	{ src = "https://github.com/folke/lazy.nvim.git", version = vim.version.range("*") },
+	{ src = 'https://github.com/folke/lazy.nvim.git', version = vim.version.range('*') },
 }, { confirm = false })
 
-require("lazy").setup({})
+require('lazy').setup({
+	{
+		'edte/copilot',
+	},
+})
