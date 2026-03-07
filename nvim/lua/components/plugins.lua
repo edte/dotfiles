246d4e8458282cb2ce4676d5443488e6d455b680
edte <zzzzip6@gmail.com>
2026-03-07 18:51:08 +0800

    feat: 新增 juan-logs.nvim 插件实现高效大文件日志浏览

--- nvim/lua/components/plugins.lua
+++ nvim/lua/components/plugins.lua
@@ -1123,6 +1123,27 @@ M.list = {
 	-- 	end,
 	-- },
 
+	{
+		'minigian/juan-logs.nvim',
+		build = function(plugin)
+			local path = plugin.dir .. '/build.lua'
+			if vim.fn.filereadable(path) == 1 then
+				dofile(path)
+			end
+		end,
+		-- You can use `build = "cargo build --release"` if you have `cargo` in your system
+		config = function()
+			require('juanlog').setup({
+				threshold_size = 1024 * 1024 * 1, -- 1MB
+				mode = 'dynamic',
+				lazy = true, -- background indexing. prevents neovim from freezing on 50GB files
+				patterns = { '*.log', '*.txt', '*.csv', '*.json' }, -- Use the plugin for these filetypes
+				enable_custom_statuscol = true, -- fakes absolute line numbers
+				syntax = false, -- set to true to enable native vim syntax (can be slow on huge files)
+			})
+		end,
+	},
+
 	-- llm
 	{
 		'olimorris/codecompanion.nvim',
