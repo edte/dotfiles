52b053c4352f7beb1994f982e823dfaf19263f96
edte <zzzzip6@gmail.com>
2026-03-13 17:02:58 +0800

    chore: 移除 fyler.nvim 插件配置及快捷键、视图与映射设置

--- nvim/lua/vim/plugins.lua
+++ nvim/lua/vim/plugins.lua
@@ -406,56 +406,6 @@ M.list = {
 			},
 		},
 	},
-
-	-- {
-	-- 	"A7Lavinraj/fyler.nvim",
-	-- 	dependencies = { "nvim-mini/mini.icons" },
-	-- 	cmd = { "Fyler" },
-	-- 	keys = {
-	-- 		{
-	-- 			"<space>e",
-	-- 			function()
-	-- 				require("fyler").toggle({
-	-- 					kind = "split_left_most",
-	-- 				})
-	-- 			end,
-	-- 		},
-	-- 	},
-	-- 	opts = {
-	-- 		integrations = {
-	-- 			-- icon = "nvim_web_devicons",
-	-- 		},
-	--
-	-- 		mappings = {
-	-- 			explorer = {
-	-- 				["<CR>"] = "Select",
-	-- 				["<C-t>"] = "SelectTab",
-	-- 				["|"] = "SelectVSplit",
-	-- 				["-"] = "SelectSplit",
-	-- 				["h"] = "GotoParent",
-	-- 				["="] = "GotoCwd",
-	-- 			},
-	-- 		},
-	-- 		views = {
-	-- 			explorer = {
-	-- 				close_on_select = true,
-	-- 				confirm_simple = true,
-	-- 				default_explorer = true,
-	-- 				win = {
-	-- 					kind_presets = {
-	-- 						split_left_most = {
-	-- 							width = "0.15rel",
-	-- 						},
-	-- 					},
-	-- 					win_opts = {
-	-- 						number = false,
-	-- 						relativenumber = false,
-	-- 					},
-	-- 				},
-	-- 			},
-	-- 		},
-	-- 	},
-	-- },
 }
 
 return M
