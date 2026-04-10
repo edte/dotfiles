fdc657d3bb6ae7d9a639c857da20b0d2f46b984f
edte <zzzzip6@gmail.com>
2026-04-10 16:33:45 +0800

    feat: 新增信任路径与插件，优化配置与缩进

--- nvim/lua/text/plugins.lua
+++ nvim/lua/text/plugins.lua
@@ -80,17 +80,82 @@ M.list = {
 				'gitattributes',
 			})
 
-		require('nvim-treesitter').setup({
-			install_dir = vim.fn.stdpath('data') .. '/site',
-		})
-
-		local sel = require('nvim-treesitter.incremental_selection')
-		-- Cmd+I (kitty 发送 \x1b[73;9u) → 初始化/扩大选择
-		vim.keymap.set('n', '\x1b[73;9u', sel.init_selection, { desc = 'TS: init selection' })
-		vim.keymap.set('x', '\x1b[73;9u', sel.node_incremental, { desc = 'TS: expand selection' })
-		-- Cmd+O (kitty 发送 \x1b[79;9u) → 缩小选择
-		vim.keymap.set('x', '\x1b[79;9u', sel.node_decremental, { desc = 'TS: shrink selection' })
+			require('nvim-treesitter').setup({
+				install_dir = vim.fn.stdpath('data') .. '/site',
+			})
 		end,
+	},
+
+	{
+		'utilyre/sentiment.nvim',
+		version = '*',
+		event = 'VeryLazy', -- keep for lazy loading
+		opts = {
+			-- config
+		},
+		init = function()
+			-- `matchparen.vim` needs to be disabled manually in case of lazy loading
+			vim.g.loaded_matchparen = 1
+		end,
+	},
+	{
+		'saghen/blink.pairs',
+		version = '*', -- (recommended) only required with prebuilt binaries
+
+		-- download prebuilt binaries from github releases
+		dependencies = 'saghen/blink.download',
+		-- OR build from source, requires nightly:
+		-- https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+		-- build = 'cargo build --release',
+		-- If you use nix, you can build from source using latest nightly rust with:
+		-- build = 'nix run .#build-plugin',
+
+		--- @module 'blink.pairs'
+		--- @type blink.pairs.Config
+		opts = {
+			mappings = {
+				-- you can call require("blink.pairs.mappings").enable()
+				-- and require("blink.pairs.mappings").disable()
+				-- to enable/disable mappings at runtime
+				enabled = true,
+				cmdline = true,
+				-- or disable with `vim.g.pairs = false` (global) and `vim.b.pairs = false` (per-buffer)
+				-- and/or with `vim.g.blink_pairs = false` and `vim.b.blink_pairs = false`
+				disabled_filetypes = {},
+				wrap = {
+					-- move closing pair via motion
+					['<C-b>'] = 'motion',
+					-- move opening pair via motion
+					['<C-S-b>'] = 'motion_reverse',
+					-- set to 'treesitter' or 'treesitter_reverse' to use treesitter instead of motions
+					-- set to nil, '' or false to disable the mapping
+					-- normal_mode = {} <- for normal mode mappings, only supports 'motion' and 'motion_reverse'
+				},
+				-- see the defaults:
+				-- https://github.com/Saghen/blink.pairs/blob/main/lua/blink/pairs/config/mappings.lua#L52
+				pairs = {},
+			},
+			highlights = {
+				enabled = true,
+				-- requires require('vim._extui').enable({}), otherwise has no effect
+				cmdline = true,
+				-- set to { 'BlinkPairs' } to disable rainbow highlighting
+				groups = { 'BlinkPairsOrange', 'BlinkPairsPurple', 'BlinkPairsBlue' },
+				unmatched_group = 'BlinkPairsUnmatched',
+
+				-- highlights matching pairs under the cursor
+				matchparen = {
+					enabled = true,
+					-- known issue where typing won't update matchparen highlight, disabled by default
+					cmdline = false,
+					-- also include pairs not on top of the cursor, but surrounding the cursor
+					include_surrounding = false,
+					group = 'BlinkPairsMatchParen',
+					priority = 250,
+				},
+			},
+			debug = false,
+		},
 	},
 
 	-- 显示代码上下文,包含函数签名
